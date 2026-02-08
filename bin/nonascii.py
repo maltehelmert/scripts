a884f1ffb692ff66eec7ba70cf16a5ab36c36a80
Malte Helmert <malte.helmert@unibas.ch>
2026-02-08 13:53:35 +0100

    nonascii.py: convert from Python2.7 to Python3

--- bin/nonascii.py
+++ bin/nonascii.py
@@ -1,5 +1,4 @@
-#! /usr/bin/env python2.7
-# -*- coding: utf-8 -*-
+#! /usr/bin/env python3
 
 import sys
 
@@ -8,42 +7,42 @@ def report(infile, prefix):
     offending_lines = []
     for line_no, line in enumerate(infile):
         try:
-            unicode(line)
+            line.decode("ascii")
         except UnicodeDecodeError:
             offending_lines.append(line)
-            print "%s%6d: %r" % (prefix, line_no + 1, line)
+            print(f"{prefix}{line_no + 1:6d}: {line!r}")
 
     if offending_lines:
-        print "-" * 72
-        print "offending lines found: trying to convert to UTF-8"
+        print("-" * 72)
+        print("offending lines found: checking if they work as UTF-8")
         all_ok = True
         offending_characters = set()
         for line in offending_lines:
             try:
-                conv_line = unicode(line, "utf-8")
+                conv_line = line.decode("utf-8")
             except UnicodeDecodeError:
-                print "failed: %r" % line
+                print(f"failed: {line!r}")
                 all_ok = False
             else:
                 for char in conv_line:
                     if ord(char) >= 127:
                         offending_characters.add(char)
         if all_ok:
-            print "OK."
-            print "-" * 72
-            print "non-ASCII characters found:"
+            print("OK.")
+            print("-" * 72)
+            print("non-ASCII characters found:")
             for char in sorted(offending_characters, key=ord):
-                print "%r = #%d = %s" % (char, ord(char), char)
+                print(f"{char!r} = #{ord(char)} = {char}")
 
 
 def main():
     args = sys.argv[1:]
     if args:
         for filename in args:
-            with open(filename) as infile:
-                report(infile, "%s: " % filename)
+            with open(filename, "rb") as infile:
+                report(infile, f"{filename}: ")
     else:
-        report(sys.stdin, "")
+        report(sys.stdin.buffer, "")
 
 
 if __name__ == "__main__":
